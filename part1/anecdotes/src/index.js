94e0742cc829f38de1d4f45371c5ff17a00e813e
Seth  Pennebaker <penne@Seths-MacBook-Pro.local>
2021-02-10 18:55:51 -0500

    some extra work today finishing up the exercise questions

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const handleNextClick = () => {
+    var array = props.size
+    var random = Math.floor(Math.random() * (array - 0) + 0)
+    setSelected(random)
+}
+  const handleVoteClick = (props) => {
+      props.votes[selected] += 1
+  }
+
+  return (
+    <div>
+      {props.anecdotes[selected]}
+      <p>has {props.votes[selected]} votes</p>
+      <Button onClick={handleVoteClick(selected)} text='Vote'/>
+      <Button onClick={handleNextClick} text='Next'/>
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+
+
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+var array = anecdotes.length
+
+let votes = Array.apply(null, new Array(10)).map(Number.prototype.valueOf,0);
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} size={array} votes={votes}/>,
+  document.getElementById('root')
+)
+
